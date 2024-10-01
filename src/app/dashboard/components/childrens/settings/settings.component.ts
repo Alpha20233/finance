938e8cad0f4cfe42b825fdae716f9ba46b8a7fa0
Alpha20233 <143025952+Alpha20233@users.noreply.github.com>
2024-10-01 16:47:12 +0530

    setting page completed

--- src/app/dashboard/components/childrens/settings/settings.component.ts
+++ src/app/dashboard/components/childrens/settings/settings.component.ts
@@ -1,14 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { AvatarComponent } from "../../../../shared/components/avatar/avatar.component";
 import { IconComponent } from "../../../../shared/components/icon/icon.component";
+import { ButtonComponent } from "../../../../shared/components/button/button.component";
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { LottieSerService } from '../../../../shared/services/lottie/lottie-ser.service';
+import { InputComponent } from '../../../../shared/components/input/input.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, AvatarComponent, IconComponent],
+  imports: [CommonModule, AvatarComponent, IconComponent, ButtonComponent,LottieComponent,InputComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+  public readonly lottieService = inject(LottieSerService);
+
+  options: AnimationOptions[] = [
+    {
+      path: '/json/edit.json',
+      autoplay: false,
+      loop: false,
+    },
+    {
+      path: '/json/check.json',
+      autoplay: false,
+      loop: false,
+    },
+  ];
+
+  isEdit = signal<boolean>(false);
+}
